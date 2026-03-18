1af94dc2e77c6029c43f3483e8efeb75391d21f3
mahimabinoj <72210014+mahimabinoj@users.noreply.github.com>
2026-03-18 20:14:13 +0000

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -12,6 +12,8 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 
+ADMIN_PASS = "12345"
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///learning.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'very-secret-key'  # Vulnerability: Hardcoded secret
